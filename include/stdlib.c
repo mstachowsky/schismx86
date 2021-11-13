d733e72e46c807caa61aee4f5c13a53da6677b54
mstachowsky <mstachowsky@gmail.com>
2021-11-12 19:46:56 -0500

    Malloc now works properly, with bytes allocated rather than uint32_t

--- include/stdlib.c
+++ include/stdlib.c
@@ -4,8 +4,6 @@
 //how big the heap is allowed to be
 void initMalloc(heapData* heap,multiBootHeader mbh)
 {
-	kernel_printf("Warning: Malloc currently only allocates uint32_ts.\n");
-	kernel_printf("TODO: Fix this to allocate bytes\n");
 	//OK, this is fairly "simple" - find the memory region that contains the
 	//heap bottom, which is a global, and then return it
 	
@@ -49,22 +47,22 @@ void initMalloc(heapData* heap,multiBootHeader mbh)
 	}
 }
 
-//TODO: Fix this to use uint8_ts and cast UP to uint32_ts
-//so that malloc actually allocates bytes
+//allocates size bytes on the heap
 void* malloc(uint32_t size)
 {
 	//begin at the first block
 	uint32_t* block = (uint32_t*)masterHeap.heapStart;
+	uint8_t* byteBlock = (uint8_t*)block;
 	bool memFound = false;
 	while(!memFound)
 	{
-		uint32_t blockSize = *(block);
-		if(blockSize >= size && *(block+2) == FREE_MEMORY) //we've found a block
+		uint32_t blockSize = block[0];
+		if(blockSize >= size && block[2] == FREE_MEMORY) //we've found a block
 		{
 			//set the size of the current block and allocate it
-			*(block) = size;
+			block[0] = size;
 			//kernel_printf("Block location: %u, Real size: %u\n",block,size);
-			*(block + 2) = ALLOCATED_MEMORY; //memory is in use
+			block[2] = ALLOCATED_MEMORY; //memory is in use
 			
 			//Make this block smaller if we can store anything in it
 			if(blockSize - size > 3)
@@ -74,15 +72,15 @@ void* malloc(uint32_t size)
 				//the next block will be at block + size + 3 because 
 				//size, next pointer, and allocation status live in memory as well
 				
-				*(block+size+3) = blockSize - size - 3; //set the new size
-				*(block+size+4) = *(block + 1); //set the next address of the next block
+				*(uint32_t*)(byteBlock+size+3*sizeof(uint32_t)) = blockSize - size - 3; //set the new size
+				*(uint32_t*)(byteBlock+size+4*sizeof(uint32_t)) = *(block + 1); //set the next address of the next block
 				//to the next address of this block
 				
 				//we know for sure that this is free memory, so set it as such
-				*(block+size+5) = FREE_MEMORY;
+				*(uint32_t*)(byteBlock+size+5*sizeof(uint32_t)) = FREE_MEMORY;
 				
 				//point this block to it
-				*(block + 1) = (block+size+3);
+				*(block + 1) = (byteBlock+size+3*sizeof(uint32_t));
 				
 			}
 			
@@ -94,13 +92,17 @@ void* malloc(uint32_t size)
 		else
 		{
 			//we need the next block
-			if(*(block+1) != 0) //this indicates there is no more memory
+			if(*(block+1) != 0) //this indicates there is more memory
+			{
 				block = *(uint32_t*)(block+1); //move to the next pointer
+				byteBlock = (uint8_t*)block;
+			}
 			else
 				return (void*)0; //fail! We are out of memory. 
 		}
 	}
 }
+
 
 void free(void* ptr)
 {
